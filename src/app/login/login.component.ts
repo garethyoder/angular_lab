429fb1dee127629aa2a9cbafa49ae71ee0b82652
Gareth Yoder <garethyoder@yahoo.com>
2022-03-15 20:15:23 -0400

    add login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  welcomeMessage = "Login Page"
+  userId="Enter User Id"
+  password=""
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  signIn() {
+    if (this.userId == "admin" && this.password == "admin") {
+      this.router.navigateByUrl("/welcome")
+    } else {
+      console.log(this.password)
+      this.welcomeMessage = "Invalid Credentials"
+    }
+  }
+
+  HEROES = [
+    {id: 1, name:'Superman'},
+    {id: 2, name:'Batman'},
+    {id: 5, name:'BatGirl'},
+    {id: 3, name:'Robin'},
+    {id: 4, name:'Flash'}
+  ];
 }
